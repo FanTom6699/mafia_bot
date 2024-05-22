9879359887f12b18fdcc0907a661aa6242d10174
kl0k3r <151632966+kl0l3r@users.noreply.github.com>
2024-05-22 20:27:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,28 +8,20 @@ from game import (
     handle_vote, players, game_in_progress, check_player_count, monitor_inactivity, update_last_active, bot_instance
 )
 
-API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+API_TOKEN = '6856455548:AAFetLGe-1CRBAT_w4J8iDbJ5qfTKfr2gEo'
 bot = telebot.TeleBot(API_TOKEN)
 bot_instance = bot
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_game_data()
 
-
 @bot.message_handler(commands=['start'])
-def start_game(message):
-    global game_in_progress
+def start(message):
     chat_id = message.chat.id
-    if game_in_progress:
-        bot.send_message(chat_id, "Игра уже идет.")
-        return
-
-    players.clear()
-    game_in_progress = False
     bot.send_message(chat_id, "Игра 'Мафия' начинается! Все желающие присоединиться, напишите /join.")
-
 
 @bot.message_handler(commands=['join'])
 def join(message):
@@ -55,33 +47,27 @@ def join(message):
         bot.send_message(chat_id, f"{message.from_user.first_name} присоединился к игре.")
         save_game_data()
 
-
 @bot.message_handler(commands=['begin'])
 def begin_game(message):
     chat_id = message.chat.id
     if not game_in_progress and check_player_count(chat_id, bot):
         start_new_game(chat_id, bot)
 
-
 @bot.callback_query_handler(func=lambda call: call.data.startswith('night_'))
 def handle_night_action(call):
     handle_night_action_callback(call, bot)
-
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('vote_'))
 def handle_vote_action(call):
     handle_vote(call, bot)
 
-
-# обновление мониторинга активности
-def update_last_active(player_id):
-    if player_id in players:
-        players[player_id]['last_active'] = datetime.now()
-
-
 @bot.message_handler(func=lambda message: True)
 def handle_all_messages(message):
     update_last_active(message.from_user.id)
+
+
+inactivity_thread = threading.Thread(target=monitor_inactivity, daemon=True)
+inactivity_thread.start()
 
 
 bot.polling(none_stop=True)
