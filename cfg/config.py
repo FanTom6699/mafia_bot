44b1291e88813a5be32f1b3a9032dad22d149a97
FanTom6699 <123177723+FanTom6699@users.noreply.github.com>
2025-07-15 11:00:20 +0500

    Update config.py

--- cfg/config.py
+++ cfg/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = os.getenv('7891995522:AAG_i8k8MtCSXP9SltFhDxaBiS5EQI-rc3w')
+API_TOKEN = os.getenv('API_TOKEN')
 
 INACTIVITY_TIMEOUT = 300
 
@@ -12,10 +12,9 @@ MIN_USER_IN_GAME = 5
 MAX_USER_IN_GAME = 8
 LOSE_MAFIA = 0
 
-HOME_DIR = '/home/student/mafia_bot'
-DB_NAME_SQLITE = f"{HOME_DIR}/database.db"
-DB_NAME_JSON = f'{HOME_DIR}/data_base.json'
-
+HOME_DIR = os.path.dirname(os.path.abspath(__file__))  # относительный путь
+DB_NAME_SQLITE = os.path.join(HOME_DIR, "../database.db")
+DB_NAME_JSON = os.path.join(HOME_DIR, "../data_base.json")
 
 MARKUP_TG = types.InlineKeyboardMarkup()
-MARKUP_TG.add(types.InlineKeyboardButton(text="🤖💬", url='https://t.me/online_mafia_game_bot'))
+MARKUP_TG.add(types.InlineKeyboardButton(text="🤖💬", url='https://t.me/TopMafiozi_bot'))
